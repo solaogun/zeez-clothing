aed41b324ac1f996c732c42f3f7e498d8bc4b842
Saidat Olaogun <solaogun@softalliance.co.ng>
2021-02-10 12:40:08 +0100

    updating our app with a shop page , collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE</h1>
-  </div>
-)
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// )
+// const JacketsPage = () => (
+//   <div>
+//     <h1>JACKETS PAGE</h1>
+//   </div>
+// )
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
-        <Route path='/shop/jackets' component={JacketsPage} />
+        <Route path='/shop/hats' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import CollectionItem from '../../components/collection-item/collection-item.component'
+import './collection-preview.style.scss'
+
+const CollectionPreview = ({ title, items }) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className='preview'>
+            {
+                items
+                    .filter((item, idx) => idx < 4)
+                    .map(({ id, ...otherCollectionProps }) => (
+                        <CollectionItem key={id} {...otherCollectionProps} />))
+            }
+        </div>
+
+    </div>
+)
+
+
+export default CollectionPreview
