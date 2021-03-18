d76d1d4b1b61133c9854bd649463a6f8cef2a5b4
Saidat Olaogun <solaogun@softalliance.co.ng>
2021-03-18 12:36:39 +0100

    created cart item component and connected cart dropdownwith cartitems reducer

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map(({ id, ...otherCollectionProps }) => (
-                        <CollectionItem key={id} {...otherCollectionProps} />))
+                    .map(item => (
+                        <CollectionItem key={item.id} item={item} />))
             }
         </div>
 
--- src/redux/cart/cart.type.jsx
+++ src/redux/cart/cart.type.jsx
@@ -1,6 +1,7 @@
 
 const CartActionType = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 }
 
 
